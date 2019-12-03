08ef64793c6a959025adb97f85a85bb70362a81b
fran <nilos@frans-iMac.local>
2019-12-03 08:37:59 -0800

    Accelerometer

--- AppSprite/AppSprite/GameScene.swift
+++ AppSprite/AppSprite/GameScene.swift
@@ -8,19 +8,21 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 var player_  = SKSpriteNode()
 var playerColor_ = UIColor.orange
 var playerSize_ = CGSize(width: 50, height: 50)
 
 class GameScene: SKScene {
     
-  
+    var motionManager = CMMotionManager()
     override func didMove(to view: SKView) {
      
         /*let recognizer = UITapGestureRecognizer(target: self, action:#selector(onetap))
         view.addGestureRecognizer(recognizer)*/
         spawnPlayer()
         addgestures()
+        
         }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
@@ -49,19 +51,37 @@ class GameScene: SKScene {
     }
     @objc func swipe(gesture:UISwipeGestureRecognizer){
         if let gesture = gesture as? UISwipeGestureRecognizer{
-            switch gesture.direction{
-            case .up:
-                print("UP")
-            case .down:
-                print("DOWN")
-            case .right:
-                print("RIGHT")
-            case .left:
-                print("LEFT")
-            default:
-                print("No")
+           switch gesture.direction{
+                      case .up:
+                          print("UP")
+                      case .down:
+                          print("DOWN")
+                      case .right:
+                          print("RIGHT")
+                      case .left:
+                          print("LEFT")
+                      default:
+                          break
             }
             
         }
     }
+        func accelerometer(){
+            
+            if motionManager.isAccelerometerAvailable{
+                motionManager.accelerometerUpdateInterval = 0.01
+                motionManager.startAccelerometerUpdates(to: .main){
+                    (data,error) in
+                    guard let data = data, error == nil else{
+                        return
+                    }
+                    if(data.acceleration.x > 30){
+                        print("Reload")
+                    }
+                    print(data.acceleration.x)
+                    
+                }
+            }
+        }
+
 }
