35a32c639889f4d85c9f07ff35c9f1321a7d5ba1
fran <nilos@frans-iMac.local>
2019-12-03 05:31:26 -0800

    Player orientation

--- AppSprite/AppSprite/GameScene.swift
+++ AppSprite/AppSprite/GameScene.swift
@@ -8,28 +8,34 @@
 
 import SpriteKit
 import GameplayKit
+var player_  = SKSpriteNode()
+var playerColor_ = UIColor.orange
+var playerSize_ = CGSize(width: 50, height: 50)
 
 class GameScene: SKScene {
     
-    let label = SKLabelNode(text: "Hello")
+  
     override func didMove(to view: SKView) {
-        addChild(label)
-        label.position = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
-        label.fontSize = 40
-        label.fontColor = SKColor.white
-        let recognizer = UITapGestureRecognizer(target: self, action:#selector(onetap))
-        view.addGestureRecognizer(recognizer)
+     
+        /*let recognizer = UITapGestureRecognizer(target: self, action:#selector(onetap))
+        view.addGestureRecognizer(recognizer)*/
+        spawnPlayer()
         }
-        
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLoc = touch.location(in: self)
+            let angle = atan2(touchLoc.y-player_.position.y, touchLoc.x-player_.position.x)
+            player_.zRotation = angle - CGFloat(Double.pi/2)
+        }
+    }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
-    @objc func onetap(recognizer: UIGestureRecognizer){
-        let viewlocation = recognizer.location(in: view)
-        let scenelocation = convertPoint(fromView: viewlocation)
-        let moveAction = SKAction.move(to: scenelocation, duration: 1)
-        label.run(moveAction)
+ 
+    func spawnPlayer(){
+        player_ = SKSpriteNode(color: playerColor_, size: playerSize_)
+        player_.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
     }
 }
