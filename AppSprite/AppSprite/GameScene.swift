4366ae4aec0fecd60d657eb04abf604e969e80ca
ASENSI DOMINGO <w9078542@tu54272.windows.tees.ac.uk>
2019-12-04 10:39:09 -0800

    Enemys and fire balls

--- AppSprite/AppSprite/GameScene.swift
+++ AppSprite/AppSprite/GameScene.swift
@@ -9,46 +9,71 @@
 import SpriteKit
 import GameplayKit
 import CoreMotion
-var player_  = SKSpriteNode()
-//var playerColor_ = UIColor.red
-//var playerSize_ = CGSize(width: 50, height: 50)
+
+
 
 class GameScene: SKScene {
-    
+    var numproj_ = 4
+    var player_  = SKSpriteNode()
+    var enemy_ = SKSpriteNode()
     var motionManager = CMMotionManager()
+    var labelprojectile_ = SKLabelNode(fontNamed: "Chalkduster")
+    let fire_ = SKSpriteNode(imageNamed: "fire")
+    
     override func didMove(to view: SKView) {
      
-        /*let recognizer = UITapGestureRecognizer(target: self, action:#selector(onetap))
-        view.addGestureRecognizer(recognizer)*/
+        
         spawnPlayer()
+        spawnEnemys(duration_: 1.2)
         addgestures()
         accelerometer()
-        
+        labelprojectile_.text = String(self.numproj_)
+        labelprojectile_.fontSize = 45
+        labelprojectile_.fontColor = SKColor.white
+        labelprojectile_.position = CGPoint(x: frame.minX + 80 , y:frame.maxY-50)
+          addChild(labelprojectile_)
+        fire_.position = CGPoint(x: frame.minX + 30 , y:frame.maxY-30)
+        addChild(fire_)
         }
-   /* override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let touchLoc = touch.location(in: self)
-            let angle = atan2(touchLoc.y-player_.position.y, touchLoc.x-player_.position.x)
-            player_.zRotation = angle - CGFloat(Double.pi/2)
-        }
-    }*/
+  
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+       
     }
  
     func spawnPlayer(){
         //player_ = SKSpriteNode(color: playerColor_, size: playerSize_)
-        let texture_ = SKTexture(imageNamed:"ship")
+        let texture_ = SKTexture(imageNamed:"dragon")
         texture_.filteringMode = SKTextureFilteringMode.nearest
 
                player_ = SKSpriteNode(texture: texture_)
         player_.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        print("aaaa")
+        
         self.addChild(player_)
         
     }
+    func spawnEnemys(duration_:Double){
+           //player_ = SKSpriteNode(color: playerColor_, size: playerSize_)
+           let texture_ = SKTexture(imageNamed:"eyelander")
+           texture_.filteringMode = SKTextureFilteringMode.nearest
+
+           enemy_ = SKSpriteNode(texture: texture_)
+           enemy_.position = CGPoint(x: self.frame.midX, y: self.frame.maxY)
+           let action = SKAction.move(to: CGPoint(
+                     x: player_.position.x,
+                     y: player_.position.y
+           ),duration: duration_)
+        let actionDone = SKAction.removeFromParent()
+        enemy_.run(SKAction.sequence([action, actionDone]))
+
+        enemy_.physicsBody = SKPhysicsBody(rectangleOf: enemy_.size)
+        enemy_.physicsBody?.affectedByGravity = false
+        enemy_.physicsBody?.isDynamic = false
+           self.addChild(enemy_)
+           
+       }
     func addgestures(){
         let gestures_: [UISwipeGestureRecognizer.Direction] = [.up, .right, .down, .left]
         for gestures  in gestures_{
@@ -61,18 +86,21 @@ class GameScene: SKScene {
         if let gesture = gesture as? UISwipeGestureRecognizer{
            switch gesture.direction{
                       case .up:
-                          
-                        player_.zRotation = 2 * CGFloat.pi
-                      case .down:
-                           player_.zRotation = CGFloat.pi
-                      case .right:
                         player_.zRotation =  3 * CGFloat.pi / 2
+                        shoot(rot: CGFloat.pi)
+                      case .down:
+                        player_.zRotation = CGFloat.pi/2
+                        shoot(rot: 2 * CGFloat.pi)
+                      case .right:
+                        player_.zRotation =   CGFloat.pi
+                        shoot(rot:  CGFloat.pi/2)
                       case .left:
-                           player_.zRotation = CGFloat.pi/2
+                        player_.zRotation = 2 * CGFloat.pi
+                        shoot(rot: 3 * CGFloat.pi / 2)
                       default:
                           break
             }
-            
+           
         }
     }
         func accelerometer(){
@@ -86,11 +114,37 @@ class GameScene: SKScene {
                     }
                     if(data.acceleration.x > -0.04){
                         print("Reload")
+                        self.numproj_ = 4
+                        self.labelprojectile_.text = String(self.numproj_)
                     }
                     print(data.acceleration.x)
                     
                 }
             }
         }
+    func shoot( rot: CGFloat) {
+        if(numproj_ > 0){
+            self.numproj_ -= 1
+            labelprojectile_.text = String(self.numproj_)
+    let projectile_ = SKSpriteNode(imageNamed: "fire")
+    projectile_.zPosition = -5
 
+    projectile_.position = player_.position
+    projectile_.zRotation = rot
+    projectile_.zPosition = player_.zPosition
+
+        let action = SKAction.move(to: CGPoint(
+            x: -700 * cos(player_.zRotation) + projectile_.position.x,
+            y: -700 * sin(player_.zRotation) + projectile_.position.y
+        ),duration: 0.8)
+    
+    let actionDone = SKAction.removeFromParent()
+        projectile_.run(SKAction.sequence([action, actionDone]))
+
+        projectile_.physicsBody = SKPhysicsBody(rectangleOf: projectile_.size)
+        projectile_.physicsBody?.affectedByGravity = false
+        projectile_.physicsBody?.isDynamic = false
+    self.addChild(projectile_)
+        }
+    }
 }
